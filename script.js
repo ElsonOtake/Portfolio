e1da07c9d66de4ab0ad0315ae4d499706de20082
Elson Otake <elsonotake@yahoo.com.br>
2022-05-09 23:19:04 -0300

    Fix linter errors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-header = document.querySelector('header');
-header.addEventListener('click', event => {
+const header = document.querySelector('header');
+const classNames = ['fa-solid', 'menu_link'];
+header.addEventListener('click', (event) => {
   const clickedElementClassName = event.target.classList[0];
-  const classNames = ['fa-solid', 'menu_link'];
-  const shouldToggle = classNames.some(className => className === clickedElementClassName);
+  const shouldToggle = classNames.some((className) => className === clickedElementClassName);
   if (shouldToggle) {
     header.classList.toggle('active');
   }
