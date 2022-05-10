77aba3dbdda07aeab8b03dcd754fa41d8ce0e20f
Elson Otake <elsonotake@yahoo.com.br>
2022-05-10 12:25:35 -0300

    Update body vertical scroll

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const header = document.querySelector('header');
 const classNames = ['fa-solid', 'menu_link'];
 header.addEventListener('click', (event) => {
@@ -5,5 +6,10 @@ header.addEventListener('click', (event) => {
   const shouldToggle = classNames.some((className) => className === clickedElementClassName);
   if (shouldToggle) {
     header.classList.toggle('active');
+    if (header.classList.contains('active')) {
+      body.classList.add('noscroll');
+    } else {
+      body.classList.remove('noscroll');
+    }
   }
 });
