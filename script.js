fbe036c39f9d4e97740c716779d195f82bd8be40
Elson Otake <elsonotake@yahoo.com.br>
2022-05-11 19:39:01 -0300

    Update js contact

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ const header = document.querySelector('header');
 // Classes for close menu
 
 const classNames = ['fa-solid', 'menu_link'];
+
 header.addEventListener('click', (event) => {
   const clickedElementClassName = event.target.classList[0];
   const shouldToggle = classNames.some((className) => className === clickedElementClassName);
+
   if (shouldToggle) {
     header.classList.toggle('active');
 
@@ -69,33 +71,44 @@ const projects = [{
   linkLiveVersion: 'https://elsonotake.github.io/odin-recipes/',
   linkSource: 'https://github.com/ElsonOtake/odin-recipes',
 }];
+
 //
 // Project sections created when page loads
 //
+
 const portfolio = document.getElementById('portfolio');
+
 for (let i = projects.length - 1; i >= 0; i -= 1) {
+
   const section = document.createElement('section');
   section.className = 'proj';
+
   const img = document.createElement('img');
   img.src = projects[i].featuredImage;
   img.alt = projects[i].altTextImage;
   section.appendChild(img);
+
   const h3 = document.createElement('h3');
   h3.className = 'themefff fw500';
   h3.innerText = projects[i].name;
   section.appendChild(h3);
+
   const p = document.createElement('p');
   p.className = 'ffinter fw400';
   p.innerText = projects[i].description;
   section.appendChild(p);
+
   const ul = document.createElement('ul');
+
   for (let k = 0; k < projects[i].technologies.length; k += 1) {
     const li = document.createElement('li');
     li.className = 'themefff ffinter fw500';
     li.innerText = projects[i].technologies[k];
     ul.appendChild(li);
   }
+
   section.appendChild(ul);
+
   const button = document.createElement('button');
   button.type = 'button';
   /* create a class with the object id number after underline */
@@ -104,12 +117,16 @@ for (let i = projects.length - 1; i >= 0; i -= 1) {
   section.appendChild(button);
   portfolio.insertAdjacentElement('afterend', section);
 }
+
 //
 // Project popup window
 //
+
 const seeProjectButtons = document.querySelectorAll('.proj button');
+
 seeProjectButtons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
+
     const i = parseInt(e.target.classList[0].substring(1), 10);
     const popupWrapper = document.createElement('div');
     popupWrapper.className = 'popup_wrapper';
@@ -122,52 +139,69 @@ seeProjectButtons.forEach((btn) => {
         popupWrapper.remove();
       }
     });
+
     const article = document.createElement('article');
     const div = document.createElement('div');
+
     const h1 = document.createElement('h1');
     h1.className = 'themefff fw500';
     h1.innerText = projects[i].name;
     div.appendChild(h1);
+
     const i0 = document.createElement('i');
     i0.className = 'popup_close themefff fa-solid fa-x';
     div.appendChild(i0);
     article.appendChild(div);
+
     const img1 = document.createElement('img');
     img1.src = projects[i].featuredImage;
     img1.alt = projects[i].altTextImage;
     article.appendChild(img1);
+
     const p2 = document.createElement('p');
     p2.className = 'themefff fw400';
     p2.innerText = projects[i].description;
     article.appendChild(p2);
+
     const ul1 = document.createElement('ul');
+
     for (let k = 0; k < projects[i].technologies.length; k += 1) {
       const li = document.createElement('li');
       li.className = 'themefff ffinter fw500';
       li.innerText = projects[i].technologies[k];
       ul1.appendChild(li);
     }
+
     article.appendChild(ul1);
+
     const div1 = document.createElement('div');
+
     const button1 = document.createElement('button');
     button1.className = 'themefff fw500';
+
     const a1 = document.createElement('a');
     a1.href = projects[i].linkLiveVersion;
+
     const span = document.createElement('span');
     span.innerText = 'See Live';
     a1.appendChild(span);
+
     const img = document.createElement('img');
     img.src = 'images/icons-live.svg';
     a1.appendChild(img);
     button1.appendChild(a1);
     div1.appendChild(button1);
+
     const button2 = document.createElement('button');
     button2.className = 'themefff fw500';
+
     const a2 = document.createElement('a');
     a2.href = projects[i].linkSource;
+
     const span1 = document.createElement('span');
     span1.innerText = 'See Source';
     a2.appendChild(span1);
+
     const i1 = document.createElement('i');
     i1.className = 'fa-brands fa-github';
     a2.appendChild(i1);
@@ -177,4 +211,72 @@ seeProjectButtons.forEach((btn) => {
     popupWrapper.appendChild(article);
     portfolio.insertAdjacentElement('afterend', popupWrapper);
   });
+});
+
+//
+// Validate contact form
+//
+
+function showMessage(input, message, type) {
+	// get the small element and set the message
+	const msg = input.parentNode.querySelector("p");
+	msg.innerText = message;
+
+	return type;
+}
+
+function validateEmail(input, invalidMsg) {
+
+	// validate email format
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+  // validate uppercase letters
+    const upperRegex = /[A-Z]/;
+	const email = input.value.trim();
+
+	if (!emailRegex.test(email) || upperRegex.test(email)) {
+
+		return showMessage(input, invalidMsg, false);
+	}
+
+	return true;
+}
+
+const formMob = document.querySelector(".form_mob");
+const formDsk = document.querySelector(".form_dsk");
+
+const EMAIL_INVALID = "Please, no uppercase letters on the email address";
+
+formMob.addEventListener("submit", function (event) {
+
+	// stop form submission
+	event.preventDefault();
+
+  // clear the message field
+  showMessage(formMob.elements["email"], "", true);
+
+	// validate the form
+	let emailValid = validateEmail(formMob.elements["email"], EMAIL_INVALID);
+
+  // if valid, submit the form.
+  if (emailValid) {
+		alert("Demo only. No form was posted.");
+	}
+});
+
+formDsk.addEventListener("submit", function (event) {
+
+	// stop form submission
+	event.preventDefault();
+
+  // clear the message field
+  showMessage(formMob.elements["email"], "", true);
+
+	// validate the form
+	let emailValid = validateEmail(formDsk.elements["email"], EMAIL_INVALID);
+
+  // if valid, submit the form.
+  if (emailValid) {
+		alert("Demo only. No form was posted.");
+	}
 });
